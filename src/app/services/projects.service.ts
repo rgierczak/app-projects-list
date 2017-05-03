f5f4f5e9bf7fda987bbd9e5d4f612fe650a05c81
Rafał Gierczak <grime85@gmail.com>
2017-05-03 13:19:57 +0200

    Added project service

---
+++ src/app/services/projects.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+
+import { Project } from '../interfaces/project';
+
+@Injectable()
+export class ProjectsService {
+    private projects: Project[] = [
+        {
+            name: 'Learn Angular Styles',
+            description: 'Practice hard to understand how you may style components',
+            status: 'active'
+        },
+        {
+            name: 'Learn Angular Animations',
+            description: 'Learn how Angular helps with animating elements on your page',
+            status: 'active'
+        },
+        {
+            name: 'Understanding Angular Basics',
+            description: 'Understand what Angular is, how it works and how and when you might use it',
+            status: 'inactive'
+        },
+        {
+            name: 'Learn JavaScript, HTML and CSS',
+            description: 'Absolutely required to dive deep into Angular and all its features',
+            status: 'critical'
+        }
+    ];
+
+    constructor() {}
+
+    loadProjects(): Observable<Project[]> {
+        return Observable.create((observer: Observer<Project[]>) => {
+            setTimeout(() => {
+                observer.next(this.projects);
+            }, 500);
+        });
+    }
+}
