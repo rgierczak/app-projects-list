880cb6e03b9363e85c20d59571b36b5a68aa5879
Rafał Gierczak <grime85@gmail.com>
2017-05-06 03:28:05 +0200

    Use multi-step transitions

--- src/app/components/projects/animations.ts
+++ src/app/components/projects/animations.ts
@@ -1,12 +1,29 @@
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 export const markedTrigger = trigger('markedState', [
     state('default', style({
         border: '1px solid #000000',
-        backgroundColor: 'transparent'
+        backgroundColor: 'transparent',
+        padding: '20px'
     })),
     state('marked', style({
         border: '2px solid #0000ff',
-        backgroundColor: '#caeff9'
-    }))
+        backgroundColor: '#caeff9',
+        padding: '19px'
+    })),
+    transition('default => marked', [
+        style({
+            border: '2px solid #000000',
+            padding: '19px'
+        }),
+        animate('300ms ease-out')
+    ]),
+    transition('marked => default', [
+        style({
+            border: '1px solid #0000ff',
+            padding: '20px'
+        }),
+        animate('300ms ease-out')
+    ]),
+    // transition('marked => default', animate('300ms ease-out')),
 ]);
